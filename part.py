0d20ffa8a32f30c9f29d5e1add223c0d32cf5352
shahzeb18 <71286737+shahzeb18@users.noreply.github.com>
2023-11-20 09:51:14 +0530

    Add files via upload

---
+++ part.py
@@ -0,0 +1,18 @@
+# def fucn1(x):
+#     y = x%10
+#     z = int((x-y)/10)
+#     print(y,z)
+
+# fucn1(67)
+
+
+
+# def func(x):
+#     if x%4==0 and (x%100!=0 or x%400==0):
+#         print('It is a leap year',x)
+#     else:
+#         print('It is not a leap year',x)
+
+# func(1904)
+
+
